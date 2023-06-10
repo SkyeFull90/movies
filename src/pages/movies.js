622adb7b6490bc342e26dea3c5f435e61bbb4218
SkyeFull90 <122103934+SkyeFull90@users.noreply.github.com>
2023-06-10 13:43:04 -0600

    update finished up some styling

--- src/pages/movies.js
+++ src/pages/movies.js
@@ -2,22 +2,29 @@ import clientPromise from "../../lib/mongodb";
 
 export default function Movies ({ movies }) {
     return (
-        <div>
+        <div className="py-10 px-10">
             <h1>Top 20 Movies of All Time</h1>
-            <p>
-              (According to Metacritic)
-            </p>
-            
-            <ul>
-                {movies.map((movie) => (
-                    <li>
-                        <figure><img src={movie.poster} alt="poster" width={200} height={200}/></figure>
-                        <h2>{movie.title}</h2>
-                        <h3>{movie.metacritic}</h3>
-                        <p>{movie.plot}</p> 
-                    </li>
-                ))}
-            </ul>
+            <p>(According to Metacritic)</p>
+            <div className="py-10 px-10">
+                <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+                    {movies.map((movie) => (
+                        <div className="mb-4">
+                            <div className="card lg:card-side bg-base-100 shadow-xl">
+                                <figure><img src={movie.poster} alt="poster" width={200} height={200}/></figure>
+                                <div className="card-body">
+                                    <h2 className="card-title">{movie.title}</h2>
+                                    <h3>{movie.metacritic}</h3>
+                                    <p>{movie.plot}</p> 
+                                    <div className="card-actions justify-end">
+                                        <button className="btn btn-primary">Watch</button>
+                                        <button className="btn btn-ghost">Trailer</button>
+                                    </div> 
+                                </div>
+                            </div>    
+                        </div>
+                    ))}
+                </div>
+           </div>         
         </div>
     );
 }   
