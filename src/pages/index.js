ff72e0a1741938e217e4c24fff746f2ff81f1962
SkyeFull90 <122103934+SkyeFull90@users.noreply.github.com>
2023-06-24 11:23:18 -0600

    changed main title

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,7 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 
-
 const inter = Inter({ subsets: ['latin'] })
-
-
 
 export default function Home() {
   return (
@@ -20,8 +17,7 @@ export default function Home() {
       <main className={`${styles.main} ${inter.className}`}>
         <div className={styles.description}>
           <p>
-            Welcome to Mflix, a sample application built with Next.js and
-            MongoDB.            
+            Welcome to Mflix            
           </p>
           <div>
             <a
