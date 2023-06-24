16e0f28cdc55f43fd041c6ae617ae8ed1cedf21d
SkyeFull90 <122103934+SkyeFull90@users.noreply.github.com>
2023-06-24 08:37:23 -0600

    updated cypress

--- cypress.config.js
+++ cypress.config.js
@@ -2,9 +2,17 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   projectId: "9dw7wz",
+
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+    },
+  },
+
+  component: {
+    devServer: {
+      framework: "next",
+      bundler: "webpack",
     },
   },
 });
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
-import clientPromise from "../../lib/mongodb";
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -42,41 +42,15 @@ export default function Home({ movies }) {
           </div>
         </div>
 
-        <div className={'py-8 px-8'}>
-        <div className="py-8 px-8 max-w-sm mx-auto bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex flex-wrap sm:items-center sm:space-y-0 sm:space-x-6">
-                <img className="block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0" src="https://images.unsplash.com/photo-1685810062916-f14fdc0bfa64?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=500&q=60" alt=" road" />
-                <div className="text-center space-y-2 sm:text-left">
-                    <div className="space-y-0.5">
-                    <p className="text-lg text-black font-semibold">
-                        Here is Top 10 Movies of All Time
-                    </p>
-                    <p className="text-slate-500 font-medium">
-                        (According to Metacritic)
-                    </p>
-                    </div>
-                </div>
-            </div> 
-            
-            <div className="py-8 px-8">
-                <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-                    {movies.map((movie) => (
-                        <div className="mb-4">
-                            <div className="card lg:card-side bg-base-100 shadow-xl">
-                                <figure><img src={movie.poster} alt="poster" width={200} height={200}/></figure>
-                                <div className="card-body">
-                                    <h2 className="card-title">{movie.title}</h2>
-                                    <h3>{movie.metacritic}</h3>
-                                    <p>{movie.plot}</p> 
-                                    <div className="card-actions justify-end">
-                                        <button className="btn btn-primary">Watch</button>
-                                        <button className="btn btn-ghost">Trailer</button>
-                                    </div> 
-                                </div>
-                            </div>    
-                        </div>
-                    ))}
-                </div>
-           </div>         
+        <div className={styles.center}>
+          <Image
+            className={styles.logo}
+            src="/next.svg"
+            alt="Next.js Logo"
+            width={180}
+            height={37}
+            priority
+          />
         </div>
 
         <div className={styles.grid}>
@@ -142,24 +116,3 @@ export default function Home({ movies }) {
   )
 }
 
-export async function getServerSideProps() {
-  try{
-      const client = await clientPromise;
-      const db = client.db("sample_mflix");
-      const movies = await db
-          .collection("movies")
-          .find({})
-          .sort({ metacritic: -1 })
-          .limit(10)
-          .toArray();
-      return {
-          props: {
-              movies: JSON.parse(JSON.stringify(movies)),
-          },
-      };
-  }
- 
-  catch (e) {
-      console.error(e);
-  }
-}
